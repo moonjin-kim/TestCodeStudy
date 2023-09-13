23d5ca0d03259e226c833cb4b35c95b2f8e0084e
moonjin-kim <ace39418@gmail.com>
2023-09-13 18:28:03 +0900

    test : order controller test

--- src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
@@ -4,12 +4,16 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class OrderCreateRequest {
 
+    @NotEmpty(message = "상품 번호 리스트는 필수입니다.")
     private List<String> productNumbers;
 
     @Builder
