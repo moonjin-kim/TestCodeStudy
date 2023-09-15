8988c6b9a5240af10aadc18385e528ba2ce135f5
moonjin-kim <ace39418@gmail.com>
2023-09-16 00:24:14 +0900

    test : OrderRepository findOrderBy function test

--- src/test/java/sample/cafekiosk/spring/domain/order/OrderRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/order/OrderRepositoryTest.java
@@ -10,9 +10,11 @@ import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
@@ -23,24 +25,52 @@ class OrderRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     @DisplayName("오늘 주문한 주문 내역을 가져온다.")
     @Test
-    void test(){
+    void findOrdersBy(){
         //given
-        LocalDateTime registeredDateTime = LocalDateTime.now();
-        List<Product> products = List.of(
-                createProduct("001",1000),
-                createProduct("002",2000)
-        );
+        Product product1 = createProduct(ProductType.BOTTLE,"001",1000);
+        Product product2 = createProduct(ProductType.BAKERY,"002",3000);
+        Product product3 = createProduct(ProductType.HANDMADE,"003",5000);
+        List<Product> products = List.of(product1,product2,product3);
+        productRepository.saveAll(products);
+
+        LocalDate startDay = LocalDate.of(2023,9,15);
+        LocalDateTime registeredDateTime = LocalDateTime.of(2023,9,15,10,0);
+
+        Order orders1 = createPaymentCompletedOrder(products,LocalDateTime.of(2023,9,14,23,59,59));
+        Order orders2 = createPaymentCompletedOrder(products, LocalDateTime.of(2023,9,15,23,59,59));
+        Order orders3 = createPaymentCompletedOrder(products, registeredDateTime);
+        Order orders4 = createPaymentCompletedOrder(products, LocalDateTime.of(2023,9,16,0,0,0));
+
+        orderRepository.save(orders1);
+        orderRepository.save(orders2);
+        orderRepository.save(orders3);
+        orderRepository.save(orders4);
 
         //when
+        List<Order> orders =orderRepository.findOrdersBy(startDay.atStartOfDay(),
+                startDay.plusDays(1).atStartOfDay(),
+                OrderStatus.PAYMENT_COMPLETED);
 
         //then
+        assertThat(orders).hasSize(2);
+    }
+    private Order createPaymentCompletedOrder(List<Product> products, LocalDateTime registeredDateTime) {
+        Order order1 = Order.builder()
+                .products(products)
+                .orderStatus(OrderStatus.PAYMENT_COMPLETED)
+                .registeredDateTime(registeredDateTime)
+                .build();
+        return orderRepository.save(order1);
     }
 
-    private Product createProduct( String productNumber, int price) {
+    private Product createProduct(ProductType type, String productNumber, int price) {
         return Product.builder()
-                .type(ProductType.HANDMADE)
+                .type(type)
                 .productNumber(productNumber)
                 .price(price)
                 .sellingStatus(ProductSellingStatus.SELLING)
